37b751b96b9cced29d16e43416591e1205d615b7
bass1121 <seanbass99@gmail.com>
2024-10-15 16:34:02 -0500

    first commit, set up project and started making journal_list.js

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { JournalList } from "./journals/journal_list";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>React, Props and State Deep Dive</h1>
+      </div>
+    );
+  }
+}
