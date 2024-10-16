a66d7509bc8331524073b3c0ce27cbd8b97a4f2e
bass1121 <seanbass99@gmail.com>
2024-10-15 20:09:34 -0500

    second commit, added functional component file journal entry and passed props to components

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Skeleton React starter Project
-
+####
+npm start to run server
 > Provided for the students of the [Bottega Code School](https://bottega.tech/)
--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -1,25 +1,30 @@
+//child component wrapped up in app
 import React, { Component } from 'react';
+import { JournalEntry } from "./journal_entry";
 
 const rawJournalData = [
     {title: "Post One", content: 'Post content', status: 'draft'},
     {title: "Post Two", content: 'Post content', status: 'published'},
     {title: "Post Three", content: 'Post content', status: 'published'},
     {title: "Post Four", content: 'Post content', status: 'published'}
-]
+];
 
+// CLASS COMPONENT
 export default class JournalList extends Component {
     constructor(props) {
         super();
 
         this.state = {
             journalData: rawJournalData,
+            greeting: "Hi there",
             isOpen: true
         }
     }
     render() {
         return (
             <div>
-                <h1></h1>
+                <h2>{this.props.greeting}</h2>
+                <JournalEntry title="TITLE" content="journal entry content" />
             </div>
         );
     }
