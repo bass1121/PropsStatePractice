165122d73f707b41e8b94bc6ac7bf142bbc0853d
bass1121 <seanbass99@gmail.com>
2024-10-16 11:41:16 -0500

    last commit. added buttons to change state, changed functions to methods, bound methods

--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -1,31 +1,64 @@
-//child component wrapped up in app
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { JournalEntry } from "./journal_entry";
 
 const rawJournalData = [
-    {title: "Post One", content: 'Post content', status: 'draft'},
-    {title: "Post Two", content: 'Post content', status: 'published'},
-    {title: "Post Three", content: 'Post content', status: 'published'},
-    {title: "Post Four", content: 'Post content', status: 'published'}
+  { title: "Post One", content: "Post content", status: "draft" },
+  { title: "Post Two", content: "More content", status: "published" },
+  { title: "Post Three", content: "More content", status: "published" },
+  { title: "Post Four", content: "More content", status: "published" }
 ];
 
-// CLASS COMPONENT
+
 export default class JournalList extends Component {
     constructor(props) {
         super();
-
+        
         this.state = {
             journalData: rawJournalData,
-            greeting: "Hi there",
             isOpen: true
-        }
+        };
+
+        this.clearEntries = this.clearEntries.bind(this);
+        this.showAllEntries = this.showAllEntries.bind(this);
+        this.toggleStatus = this.toggleStatus.bind(this);
     }
+    
+    clearEntries() {
+        this.setState({ journalData: [], isOpen: false });
+    };
+        
+    showAllEntries() {
+        this.setState({ journalData: rawJournalData, isOpen: true });
+    };
+
+    toggleStatus() {
+        if (this.state.isOpen) {
+        this.setState({ journalData: [], isOpen: false });
+        } else {
+        this.setState({ journalData: rawJournalData, isOpen: true });
+        }
+};
+
     render() {
+        const journalEntries = this.state.journalData.map(journalEntry => {
         return (
-            <div>
-                <h2>{this.props.greeting}</h2>
-                <JournalEntry title="TITLE" content="journal entry content" />
+            <div key={journalEntry.title}>
+            <JournalEntry
+                title={journalEntry.title}
+                content={journalEntry.content}
+            />
             </div>
         );
-    }
+        });
+
+    return (
+      <div>
+        <h1>{this.props.heading}</h1>
+        {journalEntries}
+        <button onClick={this.clearEntries}>Clear Journal Entries</button>
+        <button onClick={this.showAllEntries}>Show All Journal Entries</button>
+        <button onClick={this.toggleStatus}>Toggle Status</button>
+      </div>
+    );
+  }
 }
